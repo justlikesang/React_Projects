a66b75e5e9bcf46f0ff374510394e2f54276cd39
Sang <sanghosuh@gmail.com>
2022-02-23 20:58:20 -0800

    Finish movie db project

--- 21-movie-db/setup/src/SearchForm.js
+++ 21-movie-db/setup/src/SearchForm.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { useGlobalContext } from './context';
 const SearchForm = () => {
   const { query, setQuery, error } = useGlobalContext();
+  console.log(Event);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <form className="search-form" onSubmit={(e) => e.prevendefault()}>
+    <form className="search-form" onSubmit={(e) => onSubmit(e)}>
       <h2>search movies</h2>
       <input
         type="text"
